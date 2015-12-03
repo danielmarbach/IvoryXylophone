5e5c5bc5f075834008d43e1eff04bc6a2f11a7bb
anetteha <anette.hagberg@gmail.com>
2015-12-02 18:24:27 -0600

    Added project Anything

---
+++ Anything/BadAssSaga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NServiceBus;
+
+namespace Anything
+{
+    public class BadAssSaga : Saga<BadAssSaga.SagaData>
+    {
+        public class SagaData : ContainSagaData
+        {
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
