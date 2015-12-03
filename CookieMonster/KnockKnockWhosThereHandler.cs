ecff83c181a647a4cb5cdeaf636bb024c60b5c6b
Roy Cornelissen <rcornelissen@xpirit.com>
2015-12-02 19:19:33 -0600

    Fix CookieMonster and added handler

---
+++ CookieMonster/KnockKnockWhosThereHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Anything.Contracts;
+using NServiceBus;
+
+namespace CookieMonster
+{
+    public class KnockKnockWhosThereHandler: IHandleMessages<KnockKnock>
+    {
+        public Task Handle(KnockKnock message, IMessageHandlerContext context)
+        {
+            var service = new TweetService("", "", "", "");
+            service.Publish(message.Message);
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ CookieMonster/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace CookieMonster
+{
+    using System;
+    using NServiceBus;
+    using NServiceBus.Logging;
+
+    class Program
+    {
+        static void Main()
+        {
+            DoWork().GetAwaiter().GetResult();
+        }
+
+        static async Task DoWork()
+        {
+            LogManager.Use<DefaultFactory>().Level(LogLevel.Error);
+
+            var busConfiguration = new BusConfiguration();
+
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.SendFailedMessagesTo("error");
+            busConfiguration.AuditProcessedMessagesTo("audit");
+
+            busConfiguration.EnableInstallers();
+
+            var endpoint = await Endpoint.Create(busConfiguration);
+            await endpoint.Start();
+
+            Console.Out.WriteLine("CookieMonster endpoint is running, please hit any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
